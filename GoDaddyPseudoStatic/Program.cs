af6fe66af657fe408adfb85c6454e627bd3dd41a
blenderfreaky <blendcrafterxiv@gmail.com>
2021-03-12 03:48:49 +0100

    Change secret handling

--- GoDaddyPseudoStatic/Program.cs
+++ GoDaddyPseudoStatic/Program.cs
@@ -5,10 +5,6 @@ namespace GoDaddyPseudoStatic
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
     using System;
-    using System.IO;
-    using System.Linq;
-    using System.Reflection;
-    using System.Text;
     using System.Text.Json;
 
     public static class Program
@@ -22,18 +18,13 @@ namespace GoDaddyPseudoStatic
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(builder => builder.AddUserSecrets<WorkerSecrets>())
+                .ConfigureAppConfiguration(builder => builder.AddJsonFile("appsettings.apikey.json"))
                 .ConfigureLogging(loggerFactory => loggerFactory.AddEventLog().AddSimpleConsole(x => x.TimestampFormat = "[HH:mm:ss] "))
                 .ConfigureServices((hostContext, services) =>
                 {
                     IConfiguration configuration = hostContext.Configuration;
 
                     var optionsJson = configuration.GetSection("Worker").ToJson();
-
-                    File.WriteAllText("C:\\Users\\blend\\dbg2", $"{string.Join(", ", configuration.GetChildren().Select(x => x.Key))}");
-                    File.AppendAllText("C:\\Users\\blend\\dbg2", $"\n{string.Join(", ", configuration.GetSection("Worker").GetChildren().Select(x => x.Key))}");
-                    File.AppendAllText("C:\\Users\\blend\\dbg2", $"\n{Environment.CurrentDirectory}");
-                    File.WriteAllText("C:\\Users\\blend\\dbg.json", Encoding.UTF8.GetString(optionsJson));
                     var options = JsonSerializer.Deserialize<WorkerOptions>(optionsJson, new JsonSerializerOptions { IncludeFields = true, Converters = { new TimeSpanConverter() } });
                     services.AddSingleton(options);
 
