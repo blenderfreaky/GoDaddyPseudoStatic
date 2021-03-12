50532a2b27ebfdbf11e9c84c9e0fbf924b0d8645
blenderfreaky <blendcrafterxiv@gmail.com>
2021-03-12 03:27:35 +0100

    More fixes

--- GoDaddyPseudoStatic/Program.cs
+++ GoDaddyPseudoStatic/Program.cs
@@ -4,30 +4,41 @@ namespace GoDaddyPseudoStatic
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
     using System.Text.Json;
 
     public static class Program
     {
         public static void Main(string[] args)
         {
+            // This needs to be set before anything else happens so that the Hosting stuff finds the appsettings.json file
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder => builder.AddUserSecrets<WorkerSecrets>())
-                .ConfigureLogging(loggerFactory => loggerFactory.AddEventLog())
+                .ConfigureLogging(loggerFactory => loggerFactory.AddEventLog().AddSimpleConsole(x => x.TimestampFormat = "[HH:mm:ss] "))
                 .ConfigureServices((hostContext, services) =>
                 {
                     IConfiguration configuration = hostContext.Configuration;
 
-                    var options = JsonSerializer.Deserialize<WorkerOptions>(configuration.GetSection("Worker").ToJson(), new JsonSerializerOptions { IncludeFields = true, Converters = { new TimeSpanConverter() } });
+                    var optionsJson = configuration.GetSection("Worker").ToJson();
+
+                    File.WriteAllText("C:\\Users\\blend\\dbg2", $"{string.Join(", ", configuration.GetChildren().Select(x => x.Key))}");
+                    File.AppendAllText("C:\\Users\\blend\\dbg2", $"\n{string.Join(", ", configuration.GetSection("Worker").GetChildren().Select(x => x.Key))}");
+                    File.AppendAllText("C:\\Users\\blend\\dbg2", $"\n{Environment.CurrentDirectory}");
+                    File.WriteAllText("C:\\Users\\blend\\dbg.json", Encoding.UTF8.GetString(optionsJson));
+                    var options = JsonSerializer.Deserialize<WorkerOptions>(optionsJson, new JsonSerializerOptions { IncludeFields = true, Converters = { new TimeSpanConverter() } });
                     services.AddSingleton(options);
 
                     var secrets = configuration.GetSection("WorkerSecrets").Get<WorkerSecrets>();
                     services.AddSingleton(secrets);
-
-                    services.AddLogging(opt => opt.AddSimpleConsole(x => x.TimestampFormat = "[HH:mm:ss] "));
 
                     services.AddHostedService<Worker>();
                 });
--- install.ps1
+++ install.ps1
@@ -2,4 +2,6 @@ cd GoDaddyPseudoStatic
 dotnet restore
 dotnet publish -c Release -o "C:/Program Files/GoDaddyIPUpdater"
 sc.exe create GoDaddyIPUpdater binpath="C:/Program Files/GoDaddyIPUpdater/GoDaddyPseudoStatic.exe"
+sc.exe config GoDaddyIPUpdater start=auto
 sc.exe start GoDaddyIPUpdater
+cd ..
